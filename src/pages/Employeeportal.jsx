f19b78a1a187039cb92a7ebb4e2598b85e26ef23
precious1-2 <adekoyaprecious180@gmail.com>
2024-09-01 21:13:35 +0100

    employee portal

--- src/pages/Employeeportal.jsx
+++ src/pages/Employeeportal.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Login from '../Employee portal/Login'
 
 const Employeeportal = () => {
   return (
       <div>
-          
+          <Login/>
     </div>
   )
 }
